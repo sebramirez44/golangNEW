24dafd29c88695799c275270186c55ad069984f7
sebramirez44 <sebramirezcordero@gmail.com>
2024-03-21 03:32:03 -0600

    .

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"databse/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,11 +18,19 @@ type application struct {
 func main() {
 	//primero el nombre del flag, -addr, valod default, string explicando lo que hace
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:password@/snippetbox?parseTime=true", "MYSQL data source name")
 	//leemos el valor del flag, si no hacemos esto siempre es defualt
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	//inicializamos un application
 	app := &application{
@@ -35,6 +46,12 @@ func main() {
 	
 	//el valor que nos regresa flag.String es un pointer entonces le hacemos dereference.
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+
+	//como ya declaramos err ahora es asignacion y no inicializacion y asignacion (= y no :=)
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	
 }
